ac36fdfbe2e89d899b25b720b6917c7abb1a33a0
wHo-EM-i <ehteshammalik18998@gmail.com>
2021-12-14 01:17:00 +0530

    Revert "lavender: drop pixel powerhal"
    This reverts commit 26b49e86f2dcfd9ac01f673a039bb531ecad5e5f.
    
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -29,7 +29,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Soong namespaces
-PRODUCT_SOONG_NAMESPACES += device/xiaomi/lavender
+PRODUCT_SOONG_NAMESPACES += device/xiaomi/lavender \
+                            hardware/google/interfaces \
+                            hardware/google/pixel
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -325,8 +327,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.lavender-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
+
+PRODUCT_COPY_FILES += \
+	 $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
