32512bd153144bfc910515c5bc12d69106831b7c
ImPrashantt <prashant33968@gmail.com>
2021-12-16 05:11:01 +0000

    lavender: Build extphone libraries as required by Android 12 telephony
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -417,6 +417,10 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
