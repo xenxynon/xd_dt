45996e88288989d2143598f283dd477a3af19e58
ticklemykettle <ticklemykettleau@protonmail.com>
2021-12-31 02:13:45 +0600

    lavender: Bring back XiaomiDoze
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_SOONG_NAMESPACES += device/xiaomi/lavender \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
