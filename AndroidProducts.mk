f5be14e6c6a7076f9c68926518046a69262752a1
xenxynon <flynryder427@gmail.com>
2022-03-14 11:03:32 +0000

    lavender: init XD-CAF
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lavender.mk
+    $(LOCAL_DIR)/xdroid_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lavender-user \
-    aosp_lavender-userdebug \
-    aosp_lavender-eng
+    xdroid_lavender-user \
+    xdroid_lavender-userdebug \
+    xdroid_lavender-eng
--- xdroid_lavender.mk
+++ xdroid_lavender.mk
@@ -9,20 +9,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common PixelExtended stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-PEX_BUILD_TYPE := OFFICIAL
-PEX_MAINTAINER := Prashant
+# Some XD stuff presumably
+$(call inherit-product,vendor/xdroid/config/common.mk)
+XDROID_BOOT_DARK := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := xdroid_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -35,7 +30,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="coral-user 11 RQ3A.211001.001 7641976 release-keys"
 
 BUILD_FINGERPRINT :="google/coral/coral:11/RQ3A.211001.001/7641976:user/release-keys"
-
-# Pixel Extended Device Maintainers
-PRODUCT_PROPERTY_OVERRIDES += \
-    org.pex.build_maintainer=Prashant
