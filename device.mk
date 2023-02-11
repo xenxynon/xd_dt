5e77fa718906c7a5053e65c8fa02c33e960406c4
xenxynon <flynryder427@gmail.com>
2023-02-11 22:06:27 +0530

    drop ifaa
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- device.mk
+++ device.mk
@@ -232,14 +232,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl:64 \
     android.hardware.health@2.1-service
 
-
-# IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
